8c5c19196e1cc515e3332acf13610b9bd471e528
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-14 21:44:48 +0300

    deployment

--- config.py
+++ config.py
@@ -28,13 +28,13 @@ class Config:
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
     '''
     Production configuration for child class
     '''
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/blog'
-
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
     '''
@@ -44,7 +44,6 @@ class DevConfig(Config):
         SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/blog'
     DEBUG = True
 
-    DEBUG = True
 
 config_options = {
 'development':DevConfig,
