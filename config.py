f0223a68d1d694986c4e26d27f658888ff8fdcba
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-11-15 00:00:10 +0300

    deployment

--- config.py
+++ config.py
@@ -18,22 +18,21 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-    # simple mde configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    # # simple mde configurations
+    # SIMPLEMDE_JS_IIFE = True
+    # SIMPLEMDE_USE_CDN = True
 
 
-    @staticmethod
-    def init_app(app):
-        pass
+    # @staticmethod
+    # def init_app(app):
+    #     pass
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
-    '''
-    Production configuration for child class
-    '''
+   
 # class TestConfig(Config):
 #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/blog'
     
